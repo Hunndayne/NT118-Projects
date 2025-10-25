2f58d4c6131725ae7c5d18b4bb4557b49a66df50
Trần Thanh Hùng <cuteogaming@gmail.com>
2025-10-25 14:07:00 +0700

    Course Page

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 
 dependencies {
     implementation("com.kizitonwose.calendar:view:2.9.0")
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -40,4 +41,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
