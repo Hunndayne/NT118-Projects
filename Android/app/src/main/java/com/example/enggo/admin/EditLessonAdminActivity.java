f1c6f78b61e9073ae065f9c321bcd1507ba3a1f5
Ngan Tuyen Huynh <23521753@gm.uit.edu.vn>
2025-12-15 18:54:20 +0700

    front_teacher

--- Android/app/src/main/java/com/example/enggo/admin/EditLessonAdminActivity.java
+++ Android/app/src/main/java/com/example/enggo/admin/EditLessonAdminActivity.java
@@ -3,13 +3,12 @@ import com.example.enggo.R;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class EditLessonAdminActivity extends BaseAdminActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.edit_lessons_admin);
+        setContentView(R.layout.edit_lessons_teacher);
         setupAdminHeader();
         setupAdminFooter();
 
