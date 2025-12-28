6d29298d1bd9f880afb207665d90cc1c1f9d5bce
Cap Huu Tu <23521696@gm.uit.edu.vn>
2025-12-28 12:53:59 +0700

    dark mode + submission

--- Android/app/src/main/java/com/example/enggo/user/NotificationDetailActivity.java
+++ Android/app/src/main/java/com/example/enggo/user/NotificationDetailActivity.java
@@ -11,7 +11,6 @@ public class NotificationDetailActivity extends BaseUserActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notification_detail);
-        
         setupHeader();
         setupFooter();
 
